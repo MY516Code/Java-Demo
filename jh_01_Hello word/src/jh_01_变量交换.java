4d8a1feee90d5c6dbe81392bba612da1ed43e23a
MY516Code <759667898@qq.com>
2019-04-17 10:44:49 +0800

    测试

--- jh_01_Hello word/src/jh_01_变量交换.java
+++ jh_01_Hello word/src/jh_01_变量交换.java
@@ -1,7 +1,8 @@
 
 public class jh_01_变量交换 {
 	public static void main(String [] args) {
-		int a = 5;
+		//  左右手互换
+                int a = 5;
 		int b = 2;
 		System.out.println("a = "+a);
 		System.out.println("b = "+b);
